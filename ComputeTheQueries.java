e41a15e4100884c25c68205d60d7e1418d1ed547
AKARSH TIWARI <akarshat2004@gmail.com>
2024-12-19 14:09:40 +0530

    new cp problems

--- ComputeTheQueries.java
+++ ComputeTheQueries.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*given array of n integer give Q queries and in each query given number X
 print count of that number in array
 Constraints
@@ -8,6 +10,19 @@ Constraints
 public class ComputeTheQueries {
     static final int M = (int)1e9+7;
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int k=(int)1e7+10;
+        int[] hash=new int[k];
+     int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+            hash[a[i]]++;
+        }
+        int Q=sc.nextInt();
+        while (Q-->0){
+            int x=sc.nextInt();
+            System.out.println(hash[x]);
+        }
     }
 }
